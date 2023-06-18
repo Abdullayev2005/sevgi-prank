24f623753f1d7169e15bc7dce1da53a4e69863d6
Abdullayev2005 <108843760+Abdullayev2005@users.noreply.github.com>
2023-06-18 06:30:00 +0500

    nmadir

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { CAlert } from '@coreui/react';
+import { CAlertHeading } from '@coreui/react';
+import '@coreui/coreui/dist/css/coreui.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <CAlertHeading component="h4">       </CAlertHeading>
+
+      <CAlert color="dark" className>
+  <CAlertHeading component="h4">Salom Durdona</CAlertHeading>
+  <p>
+   Agar bu yozganlarimni oqiyotkan bolsang senga bir muhim gapni aytmoqchiman ozi bu gapimdan bilding nma demoqchiligimi sen juda aqli qizsan osha sen bilgan narsani aytishga men juda kop harakat qildim lekin bolmadi shuning uchun bu siteda  yozdim 
+dunyoda 3 hil sevgi bolarkan
+<hr /> 
+ 1. chiroy uchun <hr />
+ 2. organip qolish <hr />
+ 3. uchinchisi bu borligicha sevip qolish bu degani uni jiniliklari harakteri kamchiliklari va chiroyi uchun.<hr /> 
+bu gaplarimdan ensang qotishni boshlagandir iltimos shoshilmasdan ohirgacha oqi sen meni bu gaplarim uchun meni jiniga chiqarishing meni haqorat qilishing mumkin chunki men bunga loyiqman chunki men doslika hiyonat qildim boshida odiy dostlik dep oyladim lekin seni yaqindan bilgach sen meni orzuyimdagi qiz ekanligingni fahimladim seni haraktering jiniliklaring korinishing kiyinishing hamma hamasi bu gaplarimdan ozimham uyalip ketdim ozimcha adabiy yozishga harakat qilyaman meni esa borim shu togri men sen orzu qilgan bolamasdurman lekin menga birmarta imkon berishingni sorardim tarixga kelsak men sevaman dep yurgan qizlarimni shunchaki chiroyi uchun yaxshi korgan ekanman buni endi tushinib yetdim seni shoshiltirish niyatim yoq ozing bitta qarorga kelganingdan song qaroringni aytarsan senga aytmoqchi bolgan gapim 
+  </p>
+  <hr />
+  <p className="mb-0">
+  MEN SENI SEVAMAN ❤️❤️❤️❤️❤️❤️❤️
+  </p>
+</CAlert>
       </header>
     </div>
   );
